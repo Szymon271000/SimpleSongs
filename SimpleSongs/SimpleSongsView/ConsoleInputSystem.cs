9dd96a3376065a30c139fd5b155f8ffa3a9404fa
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2022-06-06 13:42:10 +0200

    Implemented ConsoleInputSystem

---
+++ SimpleSongs/SimpleSongsView/ConsoleInputSystem.cs
@@ -0,0 +1,25 @@
+﻿using SimpleSongsView.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSongsView
+{
+    public class ConsoleInputSystem : IInputSystem
+    {
+        public decimal FetchDecimalValue(string prompt)
+        {
+            decimal result;
+            while (!Decimal.TryParse(FetchStringValue(prompt), out result)) continue;
+            return result;
+        }
+
+        public string FetchStringValue(string prompt)
+        {
+            Console.WriteLine(prompt);
+            return Console.ReadLine();
+        }
+    }
+}
