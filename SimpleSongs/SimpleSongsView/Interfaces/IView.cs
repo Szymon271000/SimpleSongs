e82a0edbd897b5a6e398c41828c1875dd018f8c8
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2022-06-06 13:41:40 +0200

    Created IView

---
+++ SimpleSongs/SimpleSongsView/Interfaces/IView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSongsView.Interfaces
+{
+    public interface IView<T>
+    {
+        public void DisplayAll(List<T> entities);
+        void DisplaySingle(T entity);
+
+        public void DisplayAllSortered(List<T> entities);
+    }
+}
