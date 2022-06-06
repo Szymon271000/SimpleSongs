d298c36c53677cddad63fa0e9a19281ce960ee1e
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2022-06-06 15:46:37 +0200

    Implemented BaseHandler

--- SimpleSongs/SimpleSongs/Handler/BaseHandler.cs
+++ SimpleSongs/SimpleSongs/Handler/BaseHandler.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SimpleSongsController.Handler.Interfaces;
+using SimpleSongsView.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,36 @@ using System.Threading.Tasks;
 
 namespace SimpleSongsController.Handler
 {
-    internal class BaseHandler
+    public abstract class BaseHandler<T>: IUserDataManager
     {
+        protected readonly IInputSystem _inputSystem;
+        protected IView<T> _view;
+        protected IMenuDisplay _display;
+        protected string[] _availableCommands;
+
+        public BaseHandler(IInputSystem inputSystem, IMenuDisplay menuDisplay, IView<T> view)
+        {
+            _inputSystem = inputSystem;
+            _display = menuDisplay;
+            _view = view;
+        }
+
+        public void Run()
+        {
+            string userInput = null;
+            _display.ClearScreen();
+            while (userInput != "quit")
+            {
+                _display.PrintOptions(new List<string>(_availableCommands));
+                userInput = _inputSystem.FetchStringValue("Provide option");
+
+                RunFeatureBasedOn(userInput);
+
+                _display.PrintAndWaitForKey("\nPress enter to proceed...");
+                _display.ClearScreen();
+            }
+        }
+
+        public abstract void RunFeatureBasedOn(string userInput);
     }
 }
