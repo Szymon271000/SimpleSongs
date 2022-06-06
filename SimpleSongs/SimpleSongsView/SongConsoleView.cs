d324efba79ef3eb8b46130b045222d2f9b73a0a2
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2022-06-06 13:41:29 +0200

    Implemented SongConsoleView

---
+++ SimpleSongs/SimpleSongsView/SongConsoleView.cs
@@ -0,0 +1,28 @@
+﻿using Database.Models;
+using SimpleSongsView.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSongsView
+{
+    public class SongConsoleView : IView<Song>
+    {
+        public void DisplayAll(List<Song> entities)
+        {
+            entities.ForEach(x => DisplaySingle(x));
+        }
+
+        public void DisplayAllSortered(List<Song> entities)
+        {
+            entities.OrderBy(x => x.Title).ToList().ForEach(x => DisplaySingle(x));
+        }
+
+        public void DisplaySingle(Song entity)
+        {
+            Console.WriteLine($"{entity.Title} {entity.Author} {entity.AlbumName} {entity.Length}");
+        }
+    }
+}
