be6b33294e7039e4184a53f7be0bb8d0e0b84e74
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2022-06-06 13:41:48 +0200

    Created IInputSystem

---
+++ SimpleSongs/SimpleSongsView/Interfaces/IInputSystem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSongsView.Interfaces
+{
+    public interface IInputSystem
+    {
+        public string FetchStringValue(string prompt);
+        public decimal FetchDecimalValue(string prompt);
+    }
+}
