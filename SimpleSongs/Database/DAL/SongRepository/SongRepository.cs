1e92f43ae4e5a147b78d435d619e087461afb196
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2022-06-06 15:45:19 +0200

    Created SongRepository

---
+++ SimpleSongs/Database/DAL/SongRepository/SongRepository.cs
@@ -0,0 +1,40 @@
+﻿using Database.Context;
+using Database.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.SongRepository
+{
+    public class SongRepository : IGenericRepository<Song>
+    {
+        private readonly SongContext _songContext;
+
+        public SongRepository()
+        {
+            _songContext = new SongContext();
+        }
+        public void Delete(Song song)
+        {
+            _songContext.Songs.Remove(song);
+        }
+
+        public List<Song> GetAll()
+        {
+            return _songContext.Songs.ToList();
+        }
+
+        public void Insert(Song song)
+        {
+            _songContext.Songs.Add(song);
+        }
+
+
+        public void Save()
+        {
+            _songContext.SaveChanges();
+        }
+    }
+}
