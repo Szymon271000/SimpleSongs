15e7e170b32e0a98055f5deb088fc60f0a005ec2
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2022-06-06 15:45:02 +0200

    Created IGenericRepository

---
+++ SimpleSongs/Database/DAL/GenericRepository/Interface/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        void Insert(T obj);
+        void Delete(T song);
+        List<T> GetAll();
+        void Save();
+    }
+}
