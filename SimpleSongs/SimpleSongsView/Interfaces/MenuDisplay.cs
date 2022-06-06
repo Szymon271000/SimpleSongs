28036a94a17dcf599fc8ba6858af80e9f9533e70
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2022-06-06 15:41:12 +0200

    Implemented MenuDisplay

---
+++ SimpleSongs/SimpleSongsView/Interfaces/MenuDisplay.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSongsView.Interfaces
+{
+    public class MenuDisplay : IMenuDisplay
+    {
+        public void ClearScreen()
+        {
+            Console.Clear();
+        }
+
+        public void PrintAndWaitForKey(string content)
+        {
+            Console.WriteLine(content);
+            Console.ReadKey();
+        }
+
+        public void PrintMany<T>(List<T> entities)
+        {
+            foreach (var item in entities)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public void PrintMessage(string content)
+        {
+            Console.WriteLine(content);
+        }
+
+        public void PrintOptions(List<string> options)
+        {
+            Console.WriteLine("\n~~~~~~  O P T I O N S   A V A I L A B L E  ~~~~~~\n");
+            options.ForEach(Console.WriteLine);
+            Console.WriteLine("\nS E L E C T   O P T I O N   A N D   P R E S S   E N T E R  :\n");
+        }
+    }
+}
